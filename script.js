968a043de0f56532ec06cb4cf7e9fcf40e079cea
Laure Holemans <laure@lead-wise.com.au>
2024-12-10 13:07:38 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('budgetForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from refreshing the page
+    alert('Form submitted! Thank you for your input.');
+  });
+  
